cccc40b91348e2bdec0c2b827e408d293e0280ab
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-08-17 16:02:31 +0100

    update recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,27 +22,3 @@ end
 execute "pm2 setup" do
   command "npm install -g pm2"
 end
-
-
-# apt_repository "nginx" do
-#   uri "https://deb.nodesource.com/setup_7.x"
-#   keyserver "keyserver.ubuntu.com"
-#   distribution "nodesource_setup.sh"
-# end
-#
-# package 'nginx'
-# package 'node'
-# # include_recipe 'pm2'
-#
-# template '/etc/nginx/sites-available/default' do
-#   source 'default.erb'
-#   mode '0755'
-#   owner 'root'
-#   group 'root'
-#   notifies :restart, "service[nginx]"
-# end
-#
-# service 'nginx' do
-#   supports :restart => true, :reload => true, :start => true
-#   action [:enable, :start]
-# end
